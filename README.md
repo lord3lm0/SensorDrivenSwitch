b30b71baaeb42004b41e56fc63f47966ab24a5c1
lord3lm0 <lord3lm0@gmail.com>
2014-04-23 21:58:50 +0200

    Changed coding style

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 SensorDrivenSwitch
 ==================
 
-Arduino sketch using sensor input to control relais. Operator can change parameters via LCD.
+Arduino sketch using sensor input to control relais.
+Operator can change parameters via LCD.
+Settings are saved to eeprom.
+
--- SensorDrivenSwitch.ino
+++ SensorDrivenSwitch.ino
@@ -1,12 +1,15 @@
-/* -------------------------------------------------------------------------------------------------
- * Sensor driven switch 
- * -------------------------------------------------------------------------------------------------
+/* 
+ * SensorDrivenSwitch
+ * 
+ * Copyright (C) 2014 Almar van Merwijk
  */
 
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
 
-/* ---------------------------------------------------------------------------------------------- */
+/* 
+ * buttons from lcd panel
+ */
 #define btnRIGHT  0
 #define btnUP     1
 #define btnDOWN   2
@@ -14,73 +17,100 @@
 #define btnSELECT 4
 #define btnNONE   5
 
-/* ---------------------------------------------------------------------------------------------- */
-/* Settings */
-struct Settings {
+/* 
+ * settings as stored in eeprom 
+ */
+struct settings {
   byte sw_version;       /* When not equal, dismiss old data */
   byte threshold;        /* If sensor below this value, then turn on relais */
   byte on_time_sec;      /* Time relais is activated, in seconds */
   byte repeat_after_min; /* Time after when the sensor is evaluated, in minutes */
 };
-/* The location of the settings in eeprom memory */
+
+/* 
+ * The location/address of the settings in eeprom memory 
+ */
 #define EEPROM_SETTINGS_SW_VERSION       0
 #define EEPROM_SETTINGS_THRESHOLD        1
 #define EEPROM_SETTINGS_ON_TIME_SEC      2
 #define EEPROM_SETTINGS_REPEAT_AFTER_MIN 3
 
-/* ---------------------------------------------------------------------------------------------- */
-/* Function prototypes */
-void readSettings();
-void writeSettings();
-void fillDefaultSettings();
-void syncSettings();
-int read_LCD_buttons();
+/* 
+ * Function prototypes 
+ */
+void read_settings();
+void write_settings();
+void fill_default_settings();
+void sync_settings();
+int read_lcd_buttons();
 int read_sensor();
 
-/* ---------------------------------------------------------------------------------------------- */
-/* LCD object, select the pins used on the LCD panel */
+/* 
+ * LCD object, select the pins used on the LCD panel.
+ * The following is for a DFRobot lcd panel connected to an leonardo board.
+ */
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-/* One copy of the settings as seen in EEprom, the other as last altered by user. */
-Settings settings_eeprom, settings_current;
+/* 
+ * One copy of the settings as seen in eeprom, the other as last altered by user. 
+ */
+struct settings settings_eeprom, settings_current;
 
-// define some values used by the panel and buttons
+/* 
+ * 
+ */
 int lcd_key     = 0;
 int adc_key_in  = 0;
-int adc_moist_in = 0;
+int adc_sensor_in = 0;
 int prev_adc_key_in  = 0;
-int prev_adc_moist_in = 0;
+int prev_adc_sensor_in = 0;
 int relais = 2;
 
-/* ---------------------------------------------------------------------------------------------- */
+/*
+ * Menu item strings. Also define offset for values, so translations will be easier.
+ *                         "0123456789ABCDEF"
+ */
+const char* SENSOR_VALUE = "Sensor Read:    ";
+int SENSOR_VALUE_OFFSET = 0xC;
+const char* SWITCH_STATE = "Switch State:   ";
+int SWITCH_STATE_OFFSET = 0xD;
+
+/*
+ * Each arduino program starts with the setup.
+ */
 void setup()
 {
-  lcd.begin(16, 2);              // start the library
-  lcd.setCursor(0,0);
   pinMode(relais, OUTPUT); 
-  lcd.print("Wait For Serial"); // print a simple message
-  Serial.begin(9600);
-  while (!Serial) { //leonardo board
-    if (read_LCD_buttons() == btnSELECT) break;
-  }
-  // lcd.setCursor(0,0);
+
+  lcd.begin(16, 2);      /* start the library, 16 width, 2 high */
   lcd.clear();
-  lcd.print("GardenTech 2000 ");
+  lcd.setCursor(0,0);
+  lcd.print("Wait For Serial"); 
 
+  Serial.begin(9600);
+  while (!Serial) { /* leonardo board has no ft chip, this way we don't miss any data */
+    if (read_lcd_buttons() == btnSELECT) break; /* But if a key is pressed we stop waiting for serial */
+  }
 
-  syncSettings();
+  lcd.clear();
+  lcd.print(SENSOR_VALUE);
+  lcd.setCursor(0,1);
+  lcd.print(SWITCH_STATE);
+
+  /* Setting are read from eeprom when available */
+  sync_settings();
 }
 /* ---------------------------------------------------------------------------------------------- */
 
 void loop()
 {
-  lcd.setCursor(9,1);            // move cursor to second line "1" and 9 spaces over
-  //lcd.print(millis()/1000);      // display seconds elapsed since power-up
+  lcd.setCursor(SENSOR_VALUE_OFFSET,1);
   lcd.print( read_sensor() );
+  
   lcd.print("  ");
 
   lcd.setCursor(0,1);            // move to the begining of the second line
-  lcd_key = read_LCD_buttons();  // read the buttons
+  lcd_key = read_lcd_buttons();  // read the buttons
 
     switch (lcd_key)               // depending on which button was pushed, we perform an action
   {
@@ -121,7 +151,7 @@ void loop()
 }
 
 /* ---------------------------------------------------------------------------------------------- */
-void readSettings()
+void read_settings()
 {
   settings_eeprom.sw_version = EEPROM.read(EEPROM_SETTINGS_SW_VERSION);
   settings_eeprom.threshold = EEPROM.read(EEPROM_SETTINGS_THRESHOLD);
@@ -129,7 +159,7 @@ void readSettings()
   settings_eeprom.repeat_after_min = EEPROM.read(EEPROM_SETTINGS_REPEAT_AFTER_MIN);
 }
 /* ---------------------------------------------------------------------------------------------- */
-void writeSettings()
+void write_settings()
 {
   if (settings_eeprom.sw_version != settings_current.sw_version) {
     EEPROM.write(EEPROM_SETTINGS_SW_VERSION, settings_current.sw_version);
@@ -147,10 +177,10 @@ void writeSettings()
     EEPROM.write(EEPROM_SETTINGS_REPEAT_AFTER_MIN, settings_current.repeat_after_min);
     settings_eeprom.repeat_after_min = settings_current.repeat_after_min;
   }
-  
+
 }
 /* ---------------------------------------------------------------------------------------------- */
-void fillDefaultSettings()
+void fill_default_settings()
 {
   settings_current.sw_version = 1;
   settings_current.threshold = 0; /* Never On */
@@ -158,13 +188,13 @@ void fillDefaultSettings()
   settings_current.repeat_after_min = 60; /* 1 Hour */
 }
 /* ---------------------------------------------------------------------------------------------- */
-void syncSettings()
+void sync_settings()
 {
-  fillDefaultSettings();
-  readSettings();
+  fill_default_settings();
+  read_settings();
   /* If software is newer or when the eeprom is empty, we use default settings */
   if (settings_current.sw_version != settings_eeprom.sw_version) {
-    writeSettings();
+    write_settings();
   }
   else { /* Copy settings from eeprom to current */
     settings_current = settings_eeprom;
@@ -175,7 +205,7 @@ void syncSettings()
 }
 /* ---------------------------------------------------------------------------------------------- */
 // read the buttons
-int read_LCD_buttons()
+int read_lcd_buttons()
 {
 
   adc_key_in = analogRead(0);      // read the value from the sensor 
@@ -199,14 +229,15 @@ int read_LCD_buttons()
 /* ---------------------------------------------------------------------------------------------- */
 int read_sensor()
 {
-  adc_moist_in = analogRead(1);
-  if (abs(prev_adc_moist_in - adc_moist_in) > 2) {
+  adc_sensor_in = analogRead(1);
+  if (abs(prev_adc_sensor_in - adc_sensor_in) > 2) {
     Serial.print("Soil:");
-    Serial.println( adc_moist_in );   
-    prev_adc_moist_in = adc_moist_in;
+    Serial.println( adc_sensor_in );   
+    prev_adc_sensor_in = adc_sensor_in;
   }
-  return adc_moist_in;
+  return adc_sensor_in;
 }
 /* ---------------------------------------------------------------------------------------------- */
 /* ---------------------------------------------------------------------------------------------- */
+
 
